52e94f425c2bb58aadd5e2e33b2133dcd42450fb
sayuvvy <102788826+sayuvvy@users.noreply.github.com>
2026-06-04 07:16:50 +0530

    Add data.sql

---
+++ data.sql
@@ -0,0 +1,9 @@
+package com.migration.batch.config;
+
+import org.springframework.batch.core.configuration.annotation.DefaultBatchConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CommonBeanConfig extends DefaultBatchConfiguration {
+    // Common beans can be defined here if required
+}
